cc7720428dc4c366577a49cb9cb51ac71bf2ca6d
Francesco Michele Barranca <francescomich99@gmail.com>
2020-02-06 10:39:06 -0500

    impemented menu for navigation

---
+++ dist/js/main.js
@@ -0,0 +1,29 @@
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuBranding = document.querySelector(".menu-branding");
+
+const navItems = document.querySelectorAll(".nav-item");
+
+// Set Initial State of Menu
+let showMenu = false;
+
+menuBtn.addEventListener("click", () => {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+
+    navItems.forEach(nav => nav.classList.add("show"));
+
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+
+    navItems.forEach(nav => nav.classList.remove("show"));
+
+    showMenu = false;
+  }
+});
